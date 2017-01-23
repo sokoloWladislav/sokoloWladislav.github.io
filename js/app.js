3d0d12401b1d056735e06c33a0c07e75f180d08f
Uladzislau Sakalou <Uladzislau Sakalou>
2017-01-23 23:36:11 +0300

    add third commit

--- js/app.js
+++ js/app.js
@@ -1,19 +1,42 @@
 window.onload = function()
 {
-	let articles = document.getElementById("articles");
+	let articlesDiv = document.getElementById("articles");
 
 	let mainArticle = document.getElementById("mainArticle");
 
-	fetch("https://newsapi.org/v1/articles?source=bbc-news&apiKey=8dc451ed99104406b34432ff1c2e55a4")
+	let _getObjProxy = function(obj){
+		return new Proxy(obj, {
+			get(target, prop) {
+			    if (!target[prop]){
+			    	return undefined;
+			    }
+			    if (prop == 'publishedAt') {
+                    let date = new Date(target[prop]);
+                    return date.toLocaleString()
+                }
+			    return target[prop];
+  			}
+		});
+	}
+
+	fetch(config.apiUrl)
 		.then(responce => responce.json())
 		.then(function(data)
 		{
-			mainArticle.innerHTML = htmlMainArticle(data.articles[0]);
+			let articles = data.articles.map(article => _getObjProxy(article));
 
-			for(let i = 1; i <= data.articles.length; ++i)
+			mainArticle.innerHTML = htmlMainArticle(articles[0]);
+
+			for(let i = 1; i <= articles.length; ++i)
 			{
-				articles.innerHTML += htmlArticle(data.articles[i]);
+				articlesDiv.innerHTML += htmlArticle(articles[i]);
 			}
+			//throw new Error();
 		})
+		.catch(error => {
+			console.log(error.message);
+	});
+
+	
 
 };
--- js/newsTemplates.js
+++ js/newsTemplates.js
@@ -1,15 +1,16 @@
 function htmlMainArticle(article)
 {
-	return `<div id="mainTitle">${article.title}</div>
+	return html`<a href=${article.url} id="mainTitle">${article.title}</a>
 	<img src=${article.urlToImage}>
 	<p id="mainDescription">${article.description}</p>
+	<p id="Maindata">${article.publishedAt}</p>
 	<div class="cleaner"></div>`;
 }
 
 function htmlArticle(article)
 	{
-		return `<div class="article-container">
+		return html`<div class="article-container">
 			<img src=${article.urlToImage}>
-			<div>${article.title}</div>
+			<a href=${article.url}>${article.title}</a>
 		</div>`;
 	}
